fe54999245ac7c67cd15db8c7aa609fd5ea7f7fa
Zakepanka <arzybek@users.noreply.github.com>
2020-10-19 19:51:00 +0500

    First commit

---
+++ src/main/java/com/tvseries/TvSeries/TvSeriesApplication.java
@@ -0,0 +1,13 @@
+package com.tvseries.TvSeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvSeriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvseries/TvSeries/TvSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvseries.TvSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
